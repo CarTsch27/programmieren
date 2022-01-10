a283527e6ba6038ce259839a949c1ecdb657406f
CarTsch27 <97448867+CarTsch27@users.noreply.github.com>
2022-01-10 14:16:34 +0100

    Update Zahlen 1-1000.py

--- Zahlen 1-1000.py
+++ Zahlen 1-1000.py
@@ -1,5 +1,5 @@
-i = 1
-while i <= 1000:
-    print (i)
-    i = i + 1
+i = 1                # Variable= 1
+while i <= 1000:     # wenn Variable kleiner als 1000 ist
+    print (i)        # print Variable 1
+    i = i + 1        # Variable= 1+1
 
